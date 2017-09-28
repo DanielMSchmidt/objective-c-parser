facad70b99c2474549d5e9cd73618f94f5841fc3
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-09-29 00:12:07 +0200

    run prettier

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,90 +1,89 @@
-const fs = require('fs');
-const objcToJs = require('../index');
+const fs = require("fs");
+const objcToJs = require("../index");
 
 function loadFile(name) {
-    return fs.readFileSync(`./exampleFiles/${name}.h`, 'utf8');
+  return fs.readFileSync(`./exampleFiles/${name}.h`, "utf8");
 }
 
-describe('objective-c-parser', () => {
-    describe('basic example', () => {
-        const basic = loadFile('basic');
-        it('should return name of basic example', () => {
-            expect(objcToJs(basic).name).toBe('BasicName');
-        });
+describe("objective-c-parser", () => {
+  describe("basic example", () => {
+    const basic = loadFile("basic");
+    it("should return name of basic example", () => {
+      expect(objcToJs(basic).name).toBe("BasicName");
+    });
 
-        it('should return three methods for basic example', () => {
-            const methods = objcToJs(basic).methods;
-            expect(methods).toBeInstanceOf(Array);
-            expect(methods.length).toBe(3);
+    it("should return three methods for basic example", () => {
+      const methods = objcToJs(basic).methods;
+      expect(methods).toBeInstanceOf(Array);
+      expect(methods.length).toBe(3);
 
-            expect(methods[0].args.length).toBe(0);
-            expect(methods[0].name).toBe('basicMethodOne');
-            expect(methods[0].returnType).toBe('NSInteger');
-            expect(methods[0].comment).toBe(
-                'This is the comment of basic method one'
-            );
+      expect(methods[0].args.length).toBe(0);
+      expect(methods[0].name).toBe("basicMethodOne");
+      expect(methods[0].returnType).toBe("NSInteger");
+      expect(methods[0].comment).toBe(
+        "This is the comment of basic method one"
+      );
 
-            expect(methods[1].name).toBe('basicMethodTwoWithArgOne:AndArgTwo:');
-            expect(methods[1].returnType).toBe('NSString');
-            expect(methods[1].comment)
-                .toBe(`This is the comment of basic method two.
+      expect(methods[1].name).toBe("basicMethodTwoWithArgOne:AndArgTwo:");
+      expect(methods[1].returnType).toBe("NSString");
+      expect(methods[1].comment).toBe(`This is the comment of basic method two.
 It has multiple lines`);
 
-            expect(methods[1].args.length).toBe(2);
-            expect(methods[1].args[0].type).toBe('NSInteger');
-            expect(methods[1].args[0].name).toBe('argOne');
+      expect(methods[1].args.length).toBe(2);
+      expect(methods[1].args[0].type).toBe("NSInteger");
+      expect(methods[1].args[0].name).toBe("argOne");
 
-            expect(methods[1].args[1].type).toBe('NSString');
-            expect(methods[1].args[1].name).toBe('argTwo');
-        });
-
-        it('should be able to detect static methods', () => {
-            const methods = objcToJs(basic).methods;
-            expect(methods[0].static).toBe(false);
-            expect(methods[1].static).toBe(false);
-            expect(methods[2].static).toBe(true);
-        });
+      expect(methods[1].args[1].type).toBe("NSString");
+      expect(methods[1].args[1].name).toBe("argTwo");
     });
 
-    describe('advanced', () => {
-        const advanced = loadFile('advanced');
-
-        it('should have the right name', () => {
-            expect(objcToJs(advanced).name).toBe('GREYActions');
-        });
-
-        it('should have the right amount of methods', () => {
-            const methods = objcToJs(advanced).methods;
-            expect(methods.length).toBe(31);
-        });
-
-        it('should parse methods with pointers corretctly', () => {
-            const method = objcToJs(advanced).methods[24];
-            expect(method.name).toBe('actionForReplaceText:');
-            expect(method.args[0].type).toBe('NSString *');
-            expect(method.args[0].name).toBe('text');
-        });
-
-        it('should parse methods with underscores corretctly', () => {
-            const method = objcToJs(advanced).methods[30];
-            expect(method.name).toBe('actionForSnapshot:');
-            expect(method.args[0].type).toBe('out __strong UIImage **');
-            expect(method.args[0].name).toBe('outImage');
-        });
-
-        it('should parse methods with two args correctly', () => {
-            const method = objcToJs(advanced).methods[6];
-            expect(method.name).toBe(
-                'actionForScrollInDirection:amount:xOriginStartPercentage:yOriginStartPercentage:'
-            );
-            expect(method.args[0].type).toBe('GREYDirection');
-            expect(method.args[0].name).toBe('direction');
-            expect(method.args[1].type).toBe('CGFloat');
-            expect(method.args[1].name).toBe('amount');
-            expect(method.args[2].type).toBe('CGFloat');
-            expect(method.args[2].name).toBe('xOriginStartPercentage');
-            expect(method.args[3].type).toBe('CGFloat');
-            expect(method.args[3].name).toBe('yOriginStartPercentage');
-        });
+    it("should be able to detect static methods", () => {
+      const methods = objcToJs(basic).methods;
+      expect(methods[0].static).toBe(false);
+      expect(methods[1].static).toBe(false);
+      expect(methods[2].static).toBe(true);
     });
+  });
+
+  describe("advanced", () => {
+    const advanced = loadFile("advanced");
+
+    it("should have the right name", () => {
+      expect(objcToJs(advanced).name).toBe("GREYActions");
+    });
+
+    it("should have the right amount of methods", () => {
+      const methods = objcToJs(advanced).methods;
+      expect(methods.length).toBe(31);
+    });
+
+    it("should parse methods with pointers corretctly", () => {
+      const method = objcToJs(advanced).methods[24];
+      expect(method.name).toBe("actionForReplaceText:");
+      expect(method.args[0].type).toBe("NSString *");
+      expect(method.args[0].name).toBe("text");
+    });
+
+    it("should parse methods with underscores corretctly", () => {
+      const method = objcToJs(advanced).methods[30];
+      expect(method.name).toBe("actionForSnapshot:");
+      expect(method.args[0].type).toBe("out __strong UIImage **");
+      expect(method.args[0].name).toBe("outImage");
+    });
+
+    it("should parse methods with two args correctly", () => {
+      const method = objcToJs(advanced).methods[6];
+      expect(method.name).toBe(
+        "actionForScrollInDirection:amount:xOriginStartPercentage:yOriginStartPercentage:"
+      );
+      expect(method.args[0].type).toBe("GREYDirection");
+      expect(method.args[0].name).toBe("direction");
+      expect(method.args[1].type).toBe("CGFloat");
+      expect(method.args[1].name).toBe("amount");
+      expect(method.args[2].type).toBe("CGFloat");
+      expect(method.args[2].name).toBe("xOriginStartPercentage");
+      expect(method.args[3].type).toBe("CGFloat");
+      expect(method.args[3].name).toBe("yOriginStartPercentage");
+    });
+  });
 });
--- index.js
+++ index.js
@@ -1,105 +1,105 @@
-'use strict';
-const fs = require('fs');
+"use strict";
+const fs = require("fs");
 const methodDeclarationRegex = /(?:\+|\-)\s?\(((?:\w|\<|\>)*)\)(?:\w|\s|\:|\(|\)|\*)*;/g;
 
 // Get Groups for matches
 function getNthGroupForMatch(string, regex, index) {
-    const matches = [];
-    let match;
-    while ((match = regex.exec(string))) {
-        matches.push(match[index]);
-    }
-    return matches;
+  const matches = [];
+  let match;
+  while ((match = regex.exec(string))) {
+    matches.push(match[index]);
+  }
+  return matches;
 }
 
 const parseClassName = file => {
-    const nameRegex = /@interface \w*/i;
-    const nameRegexLength = 11;
-    return file.match(nameRegex)[0].substr(nameRegexLength);
+  const nameRegex = /@interface \w*/i;
+  const nameRegexLength = 11;
+  return file.match(nameRegex)[0].substr(nameRegexLength);
 };
 
 const extractMultiLineComment = (lineIndex, lines) => {
-    let i = lineIndex;
-    const commentLines = [];
+  let i = lineIndex;
+  const commentLines = [];
 
-    while (true) {
-        i--;
+  while (true) {
+    i--;
 
-        // is start of multi line string
-        if (lines[i].indexOf('/**') !== -1) {
-            return commentLines.join('\n');
-        }
-
-        // is end of multiline string
-        if (lines[i].indexOf('*/') !== -1) {
-            continue;
-        }
-
-        // is comment line
-        if (lines[i].indexOf('*') !== -1) {
-            commentLines.unshift(lines[i].replace(/\s*\*\s*/, ''));
-        }
+    // is start of multi line string
+    if (lines[i].indexOf("/**") !== -1) {
+      return commentLines.join("\n");
     }
+
+    // is end of multiline string
+    if (lines[i].indexOf("*/") !== -1) {
+      continue;
+    }
+
+    // is comment line
+    if (lines[i].indexOf("*") !== -1) {
+      commentLines.unshift(lines[i].replace(/\s*\*\s*/, ""));
+    }
+  }
 };
 
 const parseMethods = file => {
-    const lines = file.split('\n');
+  const lines = file.split("\n");
 
-    return file.match(methodDeclarationRegex).map(methodDeclaration => {
-        const matchReturnType = /(?:\+|\-)\s?\(((?:\w|\<|\>)*)\)/;
-        const returnType = methodDeclaration.match(matchReturnType);
+  return file.match(methodDeclarationRegex).map(methodDeclaration => {
+    const matchReturnType = /(?:\+|\-)\s?\(((?:\w|\<|\>)*)\)/;
+    const returnType = methodDeclaration.match(matchReturnType);
 
-        const methodBody = methodDeclaration
-            .replace(returnType[0], '')
-            .replace(';', '');
+    const methodBody = methodDeclaration
+      .replace(returnType[0], "")
+      .replace(";", "");
 
-        const argumentsRegex = /\s?\(((?:\w|\s|\*)*)\)((?:\w)*)\s?/g;
-        const rawArgs = [
-            getNthGroupForMatch(methodBody, argumentsRegex, 1),
-            getNthGroupForMatch(methodBody, argumentsRegex, 2),
-        ];
+    const argumentsRegex = /\s?\(((?:\w|\s|\*)*)\)((?:\w)*)\s?/g;
+    const rawArgs = [
+      getNthGroupForMatch(methodBody, argumentsRegex, 1),
+      getNthGroupForMatch(methodBody, argumentsRegex, 2)
+    ];
 
-        const name = rawArgs[0].length
-            ? methodBody
-                  .replace(argumentsRegex, '')
-                  .replace(/\s/g, '')
-                  .replace(/\(\)/g, '')
-            : methodBody;
+    const name = rawArgs[0].length
+      ? methodBody
+          .replace(argumentsRegex, "")
+          .replace(/\s/g, "")
+          .replace(/\(\)/g, "")
+      : methodBody;
 
-        const firstMethodLine = methodDeclaration.split('\n')[0];
-        const lineIndex = lines.findIndex(line => {
-            return firstMethodLine === line;
-        });
-
-        const isSingleLineComment = lines[lineIndex - 1].indexOf('//') != -1;
-        const comment = isSingleLineComment
-            ? lines[lineIndex - 1].replace(/\/\/(?:\s)*/, '')
-            : extractMultiLineComment(lineIndex, lines);
-
-        const [argumentTypes, argumentNames] = rawArgs;
-        // Both should be the same length
-        const args = argumentTypes.length
-            ? argumentTypes.map((type, index) => ({
-                  type,
-                  name: argumentNames[index],
-              }))
-            : [];
-
-        return {
-            static: methodDeclaration.indexOf('+') !== -1,
-            args,
-            comment,
-            name,
-            returnType: returnType[1],
-        };
+    const firstMethodLine = methodDeclaration.split("\n")[0];
+    const lineIndex = lines.findIndex(line => {
+      return firstMethodLine === line;
     });
+
+    const isSingleLineComment = lines[lineIndex - 1].indexOf("//") != -1;
+    const comment = isSingleLineComment
+      ? lines[lineIndex - 1].replace(/\/\/(?:\s)*/, "")
+      : extractMultiLineComment(lineIndex, lines);
+
+    const [argumentTypes, argumentNames] = rawArgs;
+    // Both should be the same length
+    const args = argumentTypes.length
+      ? argumentTypes.map((type, index) => ({
+          type,
+          name: argumentNames[index]
+        }))
+      : [];
+
+    return {
+      static: methodDeclaration.indexOf("+") !== -1,
+      args,
+      comment,
+      name,
+      returnType: returnType[1]
+    };
+  });
 };
 
 const parse = file => {
-    return {
-        name: parseClassName(file),
-        methods: parseMethods(file),
-    };
+  return {
+    name: parseClassName(file),
+    methods: parseMethods(file)
+  };
 };
 
 module.exports = parse;
